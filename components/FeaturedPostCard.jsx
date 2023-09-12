c5f88eb34518fd8c5258c7e454a43751bc18c04b
Toluwalase Ogunsola <93495428+Captain-awesome619@users.noreply.github.com>
2023-09-12 16:41:56 +0100

    slight changes

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const FeaturedPostCard = ({ post }) => (
-  <div className="relative h-72  transition duration-700 ease dark:hover:shadow-[10px_10px_10px_10px_rgba(255,255,255)] hover:shadow-[10px_10px_10px_10px_rgba(0,0,0,0.3)]">
+  <div className="relative h-72  transition duration-700 ease dark:hover:shadow-[5px_5px_5px_5px_rgba(255,255,255)] hover:shadow-[10px_10px_10px_10px_rgba(0,0,0,0.3)]">
     <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
