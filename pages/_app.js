4e23553580fcc2aacd8672eed0a9c41ad403e231
unknown <ogunsolatoluwalase@gmail.com>
2024-08-03 22:31:03 +0100

    err

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { Layout } from '../components';
-import { ThemeProvider } from "next-themes"
+import React, {useEffect, useState} from 'react';
+import Layout from "../components/Layout"
+import { ThemeProvider } from 'next-themes';
 import '../styles/globals.scss'
 
 
 function MyApp({ Component, pageProps }) {
-    return ( <
-        div className = ' dark:bg-[url("/back1.jpg")] bg-[url("/bg.jpg")] transition duration-1000 ease-in-out ' >
-        <
-        ThemeProvider attribute = "class" >
-        <
-        Layout >
-        <
-        Component {...pageProps }
-        /> <
-        /Layout> <
-        /ThemeProvider> <
-        /div>
-    );
+  return (
+<div className=' dark:bg-[url("/back1.jpg")] bg-[url("/bg.jpg")] transition duration-1000 ease-in-out'>
+<ThemeProvider attribute="class">
+<Layout >
+      <Component {...pageProps} />
+      </Layout>
+      </ThemeProvider>
+</div>
+  );
 }
 
 export default MyApp;
