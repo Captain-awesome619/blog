2f7a803830cd33d5384fd88c064a18cb7e972d3f
unknown <ogunsolatoluwalase@gmail.com>
2024-09-10 02:37:50 +0100

    icons

--- components/ThemeButton.jsx
+++ components/ThemeButton.jsx
@@ -8,9 +8,9 @@ const ThemeButton = () =>{
         <button onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")} className="cursor-pointer border-0 border-transparent focus:ring-0">
 {
     resolvedTheme === "dark" ?
-    (<BsSun className="text-[25px] text-yellow-300"/>)
+    (<BsFillMoonStarsFill className="text-[25px] text-yellow-300"/>)
     :
-    (<BsFillMoonStarsFill className="text-[25px] text-white"/>)
+    (<BsSun className="text-[25px] text-white"/>)
 }
         </button>
     )
