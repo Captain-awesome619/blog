6461321872a14a522e1c417772ef69005cdf84a5
Captain-awesome19 <ogunsolatoluwalase@gmail.com>
2026-03-16 13:57:38 +0100

    mobile

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -17,7 +17,7 @@ const PostDetails = ({ post }) => {
  
   return (
     <>
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-1 lg:px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8 ">
           {console.log(post)}
